4a41993e72327a4297bbc1de566df940eee22a03
vijayvamsi17 <chvijayvamsi@yahoo.com>
2018-07-14 17:10:10 -0400

    font awesome 5 working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+// import { FontAwesomeModule } from '@fortawesome/fontawesome-free';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
+    // FontAwesomeModule,
     AppRoutingModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
